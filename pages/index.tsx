d5149f61f5bf24cbd66eff399e95505153d05488
010pixel <010pixel@gmail.com>
2023-02-28 23:46:22 +0800

    Solved CI issues

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,34 +37,30 @@ export default function Home() {
 							})
 							.map((item: BrowserExtension) => {
 								return (
-									<>
-										<Link href={`/item/${encodeURIComponent(item.slug)}`} sx={{ textDecoration: 'none' }}>
-											<Card sx={{ width: 280, borderRadius: 3 }}>
-												<CardHeader
-													avatar={<Avatar src={item?.logo} sx={{ width: 64, height: 64 }} />}
-													title={
-														<Typography variant="body2" fontWeight={500} color="inherit">
-															{item?.name}
-														</Typography>
-													}
-													subheader={
-														<>
-															<Box sx={{ display: 'flex', flexDirection: 'row', gap: 1, mt: 1 }}>
-																{item.links.map((link: ExtensionLink) => {
-																	return (
-																		<Avatar
-																			src={`/images/browsers/${browserImageMapping[link.browser]}.svg`}
-																			sx={{ width: 16, height: 16 }}
-																		/>
-																	);
-																})}
-															</Box>
-														</>
-													}
-												/>
-											</Card>
-										</Link>
-									</>
+									<Link href={`/item/${encodeURIComponent(item.slug)}`} sx={{ textDecoration: 'none' }}>
+										<Card sx={{ width: 280, borderRadius: 3 }}>
+											<CardHeader
+												avatar={<Avatar src={item?.logo} sx={{ width: 64, height: 64 }} />}
+												title={
+													<Typography variant="body2" fontWeight={500} color="inherit">
+														{item?.name}
+													</Typography>
+												}
+												subheader={
+													<Box sx={{ display: 'flex', flexDirection: 'row', gap: 1, mt: 1 }}>
+														{item.links.map((link: ExtensionLink) => {
+															return (
+																<Avatar
+																	src={`/images/browsers/${browserImageMapping[link.browser]}.svg`}
+																	sx={{ width: 16, height: 16 }}
+																/>
+															);
+														})}
+													</Box>
+												}
+											/>
+										</Card>
+									</Link>
 								);
 							})}
 					</Grid>
