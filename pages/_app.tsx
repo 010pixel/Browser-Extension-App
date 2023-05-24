ac57a6ab42d7c3238430cb083219dfae0692352b
010pixel <010pixel@gmail.com>
2023-05-25 00:36:20 +0800

    Removed Google Adsense code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -24,11 +24,6 @@ export default function MyApp(props: MyAppProps) {
 		<CacheProvider value={emotionCache}>
 			<Head>
 				<meta name="viewport" content="initial-scale=1, width=device-width" />
-				<script
-					async
-					src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2100928988105413"
-					crossOrigin="anonymous"
-				/>
 				<script type="text/javascript">
 					var aax_size='728x90'; var aax_pubname = '010pixel-21'; var aax_src='302'; const s =
 					document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src =
