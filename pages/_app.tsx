0122c11142900b82503b6f7dd85719925f667786
010pixel <010pixel@gmail.com>
2023-05-25 00:35:16 +0800

    Removed Bidvertiser code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -29,7 +29,6 @@ export default function MyApp(props: MyAppProps) {
 					src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2100928988105413"
 					crossOrigin="anonymous"
 				/>
-				<script dangerouslySetInnerHTML={{ __html: `<!-- Bidvertiser2083409 -->` }} />
 				<script type="text/javascript">
 					var aax_size='728x90'; var aax_pubname = '010pixel-21'; var aax_src='302'; const s =
 					document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src =
