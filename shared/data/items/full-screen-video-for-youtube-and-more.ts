ae9bcac39fdf60a742fd4f20fd6a882c03e3c2dd
010pixel <010pixel@gmail.com>
2023-05-28 15:46:16 +0800

    Added change log for Full Screen Video for Youtube and more

--- shared/data/items/full-screen-video-for-youtube-and-more.ts
+++ shared/data/items/full-screen-video-for-youtube-and-more.ts
@@ -1,4 +1,4 @@
-import { BrowserExtension } from '@/common/interface';
+import { BrowserExtension, ChangeLogType } from '@/common/interface';
 
 export const fullScreenVideoForYoutubeAndMore: BrowserExtension = {
 	id: 0,
@@ -9,6 +9,86 @@ export const fullScreenVideoForYoutubeAndMore: BrowserExtension = {
 	description: '',
 	logo: 'https://lh3.googleusercontent.com/UFWLWaPyXbPa4c-MUau55XQL7JNo9HRZi7D4QvI9TOs216I5Q8QCcUhTaGlegzWRmI3ARIv_F3LzFxdOsyjY2yzGuQ=w128-h128-e365-rj-sc0x00ffffff',
 	banner: '',
+	changelog: [
+		{
+			version: '1.0.0',
+			date: '2018-06-08',
+			changes: [
+				{
+					type: ChangeLogType.Unspecified,
+					text: 'Initial upload',
+				},
+			],
+		},
+		{
+			version: '1.1.0',
+			date: '2018-06-12',
+			changes: [
+				{
+					type: ChangeLogType.Fixed,
+					text: 'Youtube home page unable to scroll',
+				},
+				{
+					type: ChangeLogType.Fixed,
+					text: 'Turn on youtube video when clicked from home page',
+				},
+				{
+					type: ChangeLogType.Added,
+					text: 'Press ESC key to switch off full screen mode',
+				},
+			],
+		},
+		{
+			version: '1.2.0',
+			date: '2018-08-13',
+			changes: [
+				{
+					type: ChangeLogType.Added,
+					text: 'added code to support latest youtube changes since July2018',
+				},
+			],
+		},
+		{
+			version: '1.2.1',
+			date: '2018-08-13',
+			changes: [
+				{
+					type: ChangeLogType.Fixed,
+					text: 'bug of click trigger not resizing video',
+				},
+			],
+		},
+		{
+			version: '1.3.0',
+			date: '2018-08-13',
+			changes: [
+				{
+					type: ChangeLogType.Added,
+					text: 'initiate extension functionality without refreshing youtube/vimeo/dailymotion page after installing extension',
+				},
+			],
+		},
+		{
+			version: '1.3.1',
+			date: '2018-08-16',
+			changes: [
+				{
+					type: ChangeLogType.Fixed,
+					text: 'youtube fullscreen css',
+				},
+			],
+		},
+		{
+			version: '1.3.2',
+			date: '2018-08-18',
+			changes: [
+				{
+					type: ChangeLogType.Fixed,
+					text: 'youtube fullscreen style',
+				},
+			],
+		},
+	],
 	links: [
 		{
 			browser: 'chrome',
