0bdd5a8db5fc6b5d02845886a961f24439a9c1d1
010pixel <010pixel@gmail.com>
2023-10-30 05:34:12 +0800

    Update AmazonBanner.tsx

--- src/components/AmazonBanner/AmazonBanner.tsx
+++ src/components/AmazonBanner/AmazonBanner.tsx
@@ -1,18 +1,20 @@
-import { Box } from '@mui/material';
 import * as React from 'react';
+import { Box } from '@mui/material';
 
 interface AmazonBannerProps {}
 
 const AmazonBanner: React.FC<AmazonBannerProps> = () => {
 	return (
-		<Box sx={{
-			width: '100%',
-			overflow:'scroll'
-		}}>
-		<div
-			style={{ textAlign: 'center', margin: '20px 0' }}
-			dangerouslySetInnerHTML={{
-				__html: `
+		<Box
+			sx={{
+				width: '100%',
+				overflow: 'scroll',
+			}}
+		>
+			<div
+				style={{ textAlign: 'center', margin: '20px 0' }}
+				dangerouslySetInnerHTML={{
+					__html: `
                     <script type="text/javascript" language="javascript">
                         var aax_size='728x90';
                         var aax_pubname = '010pixel-21';
@@ -20,8 +22,8 @@ const AmazonBanner: React.FC<AmazonBannerProps> = () => {
                     </script>
                     <script type="text/javascript" language="javascript" src="https://c.amazon-adsystem.com/aax2/assoc.js"></script>
                 `,
-			}}
-		/>
+				}}
+			/>
 		</Box>
 	);
 };
