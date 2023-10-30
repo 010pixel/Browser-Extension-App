136079bfb7c54e01e3a34f5778ddda830f91b9c5
010pixel <010pixel@gmail.com>
2023-10-31 04:09:08 +0800

    Showing notice on YouTube Video Skip Ad Trigger

--- shared/data/notices.tsx
+++ shared/data/notices.tsx
@@ -3,6 +3,7 @@ import { noticeEasyMuteForGoogleMeet } from '@/src/constants';
 const notices: { [key: string]: React.ReactNode } = {
 	'easy-mute-for-google-meet': noticeEasyMuteForGoogleMeet,
 	'google-meet-easy-mute': noticeEasyMuteForGoogleMeet,
+	'youtube-video-skip-ad-trigger': noticeEasyMuteForGoogleMeet,
 };
 
 export default notices;
